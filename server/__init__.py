475985b1de530acf2f946bcc693d2552f1469936
David Manthey <david.manthey@kitware.com>
2016-07-19 16:08:07 -0400

    If a file is connected to a database assetstore, don't allow the plugin to be disabled.

--- server/__init__.py
+++ server/__init__.py
@@ -17,26 +17,17 @@
 #  limitations under the License.
 #############################################################################
 
+import functools
+
 from girder import events
 from girder.api import access
 from girder.api.v1.assetstore import Assetstore
-from girder.constants import AccessType, AssetstoreType
+from girder.constants import AccessType, AssetstoreType, SettingKey
 from girder.utility.assetstore_utilities import setAssetstoreAdapter
 from girder.utility.model_importer import ModelImporter
 
 from . import assetstore
 from .rest import DatabaseFileResource, dbInfoKey, DatabaseAssetstoreResource
-
-
-def updateAssetstore(event):
-    params = event.info['params']
-    assetstore = event.info['assetstore']
-
-    if assetstore['type'] == AssetstoreType.DATABASE:
-        assetstore['database'] = {
-            'dbtype': params.get('dbtype', assetstore['database']['dbtype']),
-            'uri': params.get('dburi', assetstore['database']['uri'])
-        }
 
 
 @access.admin
@@ -55,6 +46,17 @@ def createAssetstore(event):
         event.preventDefault()
 
 
+def updateAssetstore(event):
+    params = event.info['params']
+    assetstore = event.info['assetstore']
+
+    if assetstore['type'] == AssetstoreType.DATABASE:
+        assetstore['database'] = {
+            'dbtype': params.get('dbtype', assetstore['database']['dbtype']),
+            'uri': params.get('dburi', assetstore['database']['uri'])
+        }
+
+
 def validateFile(event):
     """
     When a file is being validated, let the database assetstore check the
@@ -65,14 +67,42 @@ def validateFile(event):
     assetstore.validateFile(event.info)
 
 
+def validateSettings(event, plugin_name=None):
+    """
+    Validate plugin-specific settings and prevent disabling this plugin if
+    there are any files in database assetstores.
+
+    :param plugin_name: the name of our plugin.
+    :param event: the validation event
+    """
+    key, val = event.info['key'], event.info['value']
+
+    # If we are validating the list of enabled plugins, and there are any
+    # database assetstores with files, do not allow the plugin to be disabled.
+    if (key == SettingKey.PLUGINS_ENABLED and plugin_name and
+            plugin_name not in val):
+        if any(store['type'] == AssetstoreType.DATABASE and store['hasFiles']
+               for store in ModelImporter.model('assetstore').list()):
+            val.append(plugin_name)
+
+
 def load(info):
+    """
+    Load the plugin into Girder.
+
+    :param info: a dictionary of plugin information.  The name key contains the
+                 name of the plugin according to Girder.
+    """
+    plugin_name = info['name']
     AssetstoreType.DATABASE = 'database'
     setAssetstoreAdapter(AssetstoreType.DATABASE,
                          assetstore.DatabaseAssetstoreAdapter)
     events.bind('assetstore.update', 'database_assetstore', updateAssetstore)
     events.bind('rest.post.assetstore.before', 'database_assetstore',
                 createAssetstore)
-    events.bind('model.file.validate', 'worker', validateFile)
+    events.bind('model.file.validate', 'database_assetstore', validateFile)
+    events.bind('model.setting.validate', 'database_assetstore',
+                functools.partial(validateSettings, plugin_name=plugin_name))
 
     (Assetstore.createAssetstore.description
         .param('dbtype', 'The database type (for Database type).',
